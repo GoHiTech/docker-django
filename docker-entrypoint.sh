83bdd977defa237793a7a6a40546078b9616d9ea
Dean Taylor <dean@gohitech.net>
2017-11-17 17:41:59 +0800

    altered config. for django-celery

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # http://docs.gunicorn.org/en/stable/settings.html
+# https://pypi.python.org/pypi/django-celery
 
 # Sanity checks
 [ -d /docker-entrypoint.d ] || mkdir -p /docker-entrypoint.d
@@ -13,7 +14,8 @@ fi
 export DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-${DJANGO_PROJECT_NAME}.settings}"
 
 # Ensure base setting files are in location
-[ -d ${DJANGO_PROJECT_NAME}/settings.d ]  || mkdir -p ${DJANGO_PROJECT_NAME}/settings.d
+[ -d settings.d ] || mkdir settings.d
+[ -d ${DJANGO_PROJECT_NAME}/settings.d ]  || ln -sr settings.d ${DJANGO_PROJECT_NAME}/settings.d
 [ -f ${DJANGO_PROJECT_NAME}/settings.py ] && mv ${DJANGO_PROJECT_NAME}/settings.py ${DJANGO_PROJECT_NAME}/settings.d/
 ln -sr settings.py ${DJANGO_PROJECT_NAME}/settings.py
 
@@ -50,20 +52,44 @@ else
 fi
 
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-echo 'python manage.py collectstatic --noinput'
-python manage.py collectstatic --noinput
+if [[ $CELERY_ENABLE != True ]]; then
+  echo 'python manage.py collectstatic --noinput'
+  python manage.py collectstatic --noinput
+fi
+
+echo 'python manage.py migrate djcelery'
+python manage.py migrate djcelery
 
 # Source files in docker-entrypoint.d/ dump directory
-IFS=$'\n' eval 'for f in $(find /docker-entrypoint.d/ -type f -print |sort); do source ${f}; done'
+IFS=$'\n' eval 'for f in $(find /docker-entrypoint.d/ -type f ! \( -iname '*.DISABLE' \) -print |sort); do source ${f}; done'
 
-# Start Gunicorn process
-echo 'Starting Gunicorn.'
-exec gunicorn ${DJANGO_PROJECT_NAME}.wsgi \
-  --name         ${GUNICORN_NAME:-'wsgi_app'} \
-  --bind         ${GUNICORN_BIND_IP:-'0.0.0.0'}:8000 \
-  --worker-class ${GUNICORN_WORKER_CLASS:-'sync'} \
-  --workers      ${GUNICORN_WORKERS:-$(( 2 * $(nproc --all) ))} \
-  --log-level    ${GUNICORN_LOG_LEVEL:-'info'} \
-  --error-logfile - \
-  --access-logfile - \
-  "$@"
+if [[ $CELERY_ENABLE == True ]]; then
+  [ -z $CELERY_USER ] && export CELERY_USER='user'
+
+  export GUNICORN_ENABLE='False'
+
+  if [ ! -f celeryconfig.py ]; then
+    cat >celeryconfig.py <<EOT
+import os
+BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'amqp://')
+EOT
+  fi
+
+  echo 'Starting Celery worker.'
+  su -c "python manage.py celery worker $@" -p - $CELERY_USER
+elif [[ $GUNICORN_ENABLE != False ]]; then
+  # Start Gunicorn process
+  echo 'Starting Gunicorn.'
+  exec gunicorn ${DJANGO_PROJECT_NAME}.wsgi \
+    --name         ${GUNICORN_NAME:-'wsgi_app'} \
+    --bind         ${GUNICORN_BIND_IP:-'0.0.0.0'}:8000 \
+    --user         ${GUNICORN_USER:-'user'} \
+    --worker-class ${GUNICORN_WORKER_CLASS:-'sync'} \
+    --workers      ${GUNICORN_WORKERS:-$(( 2 * $(nproc --all) ))} \
+    --log-level    ${GUNICORN_LOG_LEVEL:-'info'} \
+    --access-logfile - \
+    --error-logfile - \
+    "$@"
+else
+  $@
+fi
