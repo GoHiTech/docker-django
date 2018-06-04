a1dd5e19194b0bb7471ee5b5c1afc5aefce9540d
Dean Taylor <dean@gohitech.net>
2018-06-04 18:59:43 +0800

    modify for kubernetes deployment

--- Dockerfile
+++ Dockerfile
@@ -7,19 +7,23 @@ MAINTAINER Dean Taylor <dean@gohitech.net>
 EXPOSE 8000
 ENTRYPOINT ["/docker-entrypoint.sh"]
 WORKDIR /usr/src/app
+VOLUME ["/usr/src/app/static"]
 
 RUN apt-get update && apt-get -y install \
   netcat \
-  && apt-get clean
+  && apt-get clean \
+  && pip install --no-cache-dir --upgrade pip
 
 RUN pip install --no-cache-dir \
+  celery==3.1.17 \
   Django==1.8.18 \
   envparse \
   gevent \
   gunicorn
 
 COPY docker-entrypoint.d/ /docker-entrypoint.d/
-COPY docker-entrypoint.sh /
+COPY docker-entrypoint_celery.d/ /docker-entrypoint_celery.d/
+COPY docker-entrypoint.sh /docker-entrypoint.sh
 
 # Django Docker default settings
 ENV DJANGO_ALLOWED_HOSTS="['localhost','.gohitech.net',]"
@@ -34,3 +38,5 @@ ENV DJANGO_PROJECT_NAME="gohitech"
 COPY settings.py ./settings.py
 COPY settings.d/ ./settings.d/
 
+RUN groupadd user \
+  && useradd --home-dir "$(pwd)" -g user user
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,25 +3,48 @@
 
 # Sanity checks
 [ -d /docker-entrypoint.d ] || mkdir -p /docker-entrypoint.d
-export DJANGO_PROJECT_NAME="${DJANGO_PROJECT_NAME:-mysite}"
+[ -d /docker-entrypoint_celery.d ] || mkdir -p /docker-entrypoint_celery.d
+[ -z $DJANGO_PROJECT_NAME ] && export DJANGO_PROJECT_NAME="${DJANGO_PROJECT_NAME:-mysite}"
+[ -z $RUNAS_USER ] && export RUNAS_USER='user'
 
 # Create a new project if manage.py does not exist
 if [ ! -f manage.py ]; then
   django-admin startproject ${DJANGO_PROJECT_NAME} .
   mv ${DJANGO_PROJECT_NAME}/settings.py ${DJANGO_PROJECT_NAME}/settings_startproject.py
 fi
-export DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-${DJANGO_PROJECT_NAME}.settings}"
+[ -z $DJANGO_SETTINGS_MODULE ] && export DJANGO_SETTINGS_MODULE="${DJANGO_PROJECT_NAME}.settings"
 
 # Ensure base setting files are in location
-[ -d ${DJANGO_PROJECT_NAME}/settings.d ]  || mkdir -p ${DJANGO_PROJECT_NAME}/settings.d
-[ -f ${DJANGO_PROJECT_NAME}/settings.py ] && mv ${DJANGO_PROJECT_NAME}/settings.py ${DJANGO_PROJECT_NAME}/settings.d/
-ln -sr settings.py ${DJANGO_PROJECT_NAME}/settings.py
+[ -d settings.d ]                        || mkdir -p settings.d
+[ -d ${DJANGO_PROJECT_NAME}/settings.d ] || ln -sr settings.d ${DJANGO_PROJECT_NAME}/settings.d
+if [ ! -h ${DJANGO_PROJECT_NAME}/settings.py ]; then
+  [ -f ${DJANGO_PROJECT_NAME}/settings.py ] && mv ${DJANGO_PROJECT_NAME}/settings.py ${DJANGO_PROJECT_NAME}/settings.py_$(date '+%Y%m%d')
+  ln -sr settings.py ${DJANGO_PROJECT_NAME}/settings.py
+fi
+if [ -f settings_pre.py ]; then
+  [ -h ${DJANGO_PROJECT_NAME}/settings_pre.py ] || ln -sr settings_pre.py ${DJANGO_PROJECT_NAME}/settings_pre.py
+fi
 
 # Services?
 is_memcached=false; is_db=false
-ping -c1 -w1 memcached &>/dev/null && is_memcached=true
-ping -c1 -w1 db        &>/dev/null && is_db=true
+#if [ ! \( -z $MEMCACHED_ENABLE -a -z $MEMCACHED_HOSTNAME -a -z $MEMCACHED_PORT \) ]; then
+#  is_memcached=true
+#fi
+#[ -z $MEMCACHED_PORT $MEMCACHED_HOSTNAME $MEMCACHED_ENABLE ]
+#ping -c1 -w1 memcached &>/dev/null && is_memcached=true
+#ping -c1 -w1 db        &>/dev/null && is_db=true
 export is_memcached is_db
+
+# Run?
+if [[ $GUNICORN_ENABLE == True ]]; then
+  CELERY_ENABLE='False'
+elif [[ $CELERY_ENABLE == True ]] || [[ $CELERY_ENABLE == worker ]] || [[ $CELERY_ENABLE == beat ]]; then
+  GUNICORN_ENABLE='False'
+elif [[ $GUNICORN_ENABLE != False ]]; then
+  CELERY_ENABLE='False'
+  GUNICORN_ENABLE='True'
+fi
+export CELERY_ENABLE GUNICORN_ENABLE
 
 # Configure and setup memcached if present
 if $is_memcached; then
@@ -32,38 +55,95 @@ fi
 
 # Configure and setup database if present
 if $is_db; then
-  pip install --no-cache-dir psycopg2
+  if [ -z $POSTGRES_HOSTNAME ]; then
+    case $DEPLOYMENT in
+      kubernetes)   POSTGRES_HOSTNAME='localhost' ;;
+      docker|swarm) POSTGRES_HOSTNAME='db' ;;
+      *)            POSTGRES_HOSTNAME='db' ;;
+    esac
+    export POSTGRES_HOSTNAME
+  fi
+  [ -z $POSTGRES_PASSWORD ] && export POSTGRES_PASSWORD='postgres'
+
+  pip install --no-cache-dir psycopg2-binary
 
   # Ensure Postgres database is ready to accept a connection
   echo "Trying db connection..."
-  while ! nc -z db 5432; do
+  while ! nc -z $POSTGRES_HOSTNAME 5432; do
     echo "BD not ready, will try again shortly"
     sleep 1
   done
   echo "Connected to DB, will continue processing"
-  sleep 3
+  sleep 2
 
-  $is_memcached || python manage.py createcachetable
-  python manage.py migrate
+  if [[ $GUNICORN_ENABLE == True ]]; then
+    echo 'python manage.py migrate --fake-initial'
+    python manage.py migrate --fake-initial
+
+    if ! $is_memcached; then
+      echo 'python manage.py createcachetable'
+      python manage.py createcachetable
+    fi
+  fi
 else
   echo "WARNING: Database container link; db: Name or service not known"
 fi
 
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-echo 'python manage.py collectstatic --noinput'
-python manage.py collectstatic --noinput
+if [[ $GUNICORN_ENABLE == True ]]; then
+  # Source files in docker-entrypoint.d/ dump directory
+  IFS=$'\n' eval 'for f in $(find /docker-entrypoint.d/ -type f ! \( -iname '*.DISABLE' \) -print |sort); do source ${f}; done'
 
-# Source files in docker-entrypoint.d/ dump directory
-IFS=$'\n' eval 'for f in $(find /docker-entrypoint.d/ -type f -print |sort); do source ${f}; done'
+  export GUNICORN_USER="${RUNAS_USER}"
+  # https://docs.djangoproject.com/en/1.8/howto/static-files/
+  echo 'python manage.py collectstatic --noinput'
+  python manage.py collectstatic --noinput
 
-# Start Gunicorn process
-echo 'Starting Gunicorn.'
-exec gunicorn ${DJANGO_PROJECT_NAME}.wsgi \
-  --name         ${GUNICORN_NAME:-'wsgi_app'} \
-  --bind         ${GUNICORN_BIND_IP:-'0.0.0.0'}:8000 \
-  --worker-class ${GUNICORN_WORKER_CLASS:-'sync'} \
-  --workers      ${GUNICORN_WORKERS:-$(( 2 * $(nproc --all) ))} \
-  --log-level    ${GUNICORN_LOG_LEVEL:-'info'} \
-  --error-logfile - \
-  --access-logfile - \
-  "$@"
+  # Start Gunicorn process
+  echo 'Starting Gunicorn.'
+  exec gunicorn ${DJANGO_PROJECT_NAME}.wsgi \
+    --name         ${GUNICORN_NAME:-'wsgi_app'} \
+    --bind         ${GUNICORN_BIND_IP:-'0.0.0.0'}:8000 \
+    --user         ${GUNICORN_USER:-'user'} \
+    --worker-class ${GUNICORN_WORKER_CLASS:-'sync'} \
+    --workers      ${GUNICORN_WORKERS:-$(( 2 * $(nproc --all) ))} \
+    --log-level    ${GUNICORN_LOG_LEVEL:-'info'} \
+    --access-logfile - \
+    --error-logfile - \
+    "$@"
+  exit
+elif [[ $CELERY_ENABLE == True ]] || [[ $CELERY_ENABLE == worker ]] || [[ $CELERY_ENABLE == beat ]]; then
+  # Source files in docker-entrypoint_celery.d/ dump directory
+  IFS=$'\n' eval 'for f in $(find /docker-entrypoint_celery.d/ -type f ! \( -iname '*.DISABLE' \) -print |sort); do source ${f}; done'
+
+  [ -f /tmp/celerybeat.pid ] && rm -f /tmp/celerybeat.pid
+
+  export CELERY_USER="${RUNAS_USER}"
+
+  export GUNICORN_ENABLE='False'
+
+  if [ ! -f celeryconfig.py ]; then
+    cat >celeryconfig.py <<EOT
+import os
+BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'amqp://')
+EOT
+  fi
+
+  celery_cmd='worker'
+  if [[ $CELERY_ENABLE == beat ]]; then
+    celery_cmd='beat --pidfile=/tmp/celerybeat.pid --schedule=/tmp/celerybeat-schedule'
+    if $is_db; then
+      celery_cmd="${celery_cmd} --scheduler=djcelery.schedulers.DatabaseScheduler"
+      sleep 3	# Workaround; wait for djcelery migration
+    fi
+  fi
+
+  su_cmd="python manage.py celery $celery_cmd $@"
+
+  echo 'Starting Celery...'; echo "${su_cmd}"
+  su -c "${su_cmd}" -p - $CELERY_USER
+  exit
+else
+  su_cmd="${@}"
+  su -c "${su_cmd}" -p - $RUNAS_USER
+  exit
+fi
